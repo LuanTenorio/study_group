815884d292c6f0f23e16530af1bff1135351f0dc
Luan Tenorio <lluantenorio7@gmail.com>
2026-06-22 18:42:52 -0300

    fix animation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import preset from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +16,6 @@ export const appConfig: ApplicationConfig = {
           darkModeSelector: 'none'
         } }}),
     provideHttpClient(withFetch()),
-    provideAnimationsAsync()
+    provideAnimations()
   ]
 };
