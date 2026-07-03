9dbefd6dda11e2b5319502664d2fe06014cece24
caue-oliver <caue.ooliver@gmail.com>
2026-07-03 18:58:03 -0300

    feat: implementação inicial tela feed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { GroupComponent } from './group/group.component';
+import { FeedComponent } from './feed/feed.component';
 
 export const routes: Routes = [
+    { path: '', component: FeedComponent },
+
     { path: "group/:id", component: GroupComponent },
 ];
