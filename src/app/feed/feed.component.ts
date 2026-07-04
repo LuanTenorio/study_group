dfe682ac02fe90b527a19c54ac48d3b14ac85d67
caue-oliver <caue.ooliver@gmail.com>
2026-07-04 14:56:42 -0300

    feat: visual tela de feed

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, signal } from "@angular/core";
 
 @Component({
   selector: 'app-feed',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './feed.component.html',
-  styleUrl: './feed.component.scss',
+  styleUrl: './feed.component.scss'
 })
-export class FeedComponent {}
+export class FeedComponent {
+  
+  // Categorias de Áreas de Conhecimento
+  categories = signal<area_card[]>([
+    { name: 'Exatas', icon: 'pi-calculator' },
+    { name: 'Humanas', icon: 'pi-book' },
+    { name: 'Biológicas', icon: 'pi-eye' },
+    { name: 'Tecnologia', icon: 'pi-desktop' },
+    { name: 'Idiomas', icon: 'pi-globe' },
+    { name: 'Negócios', icon: 'pi-chart-bar' },
+    { name: 'Artes', icon: 'pi-palette' },
+    { name: 'Saúde', icon: 'pi-heart' }
+  ]);
+
+  popularGroups = signal<group_card[]>([
+    {
+      title: 'Banco de Dados: SQL Avançado',
+      institution: 'UnB',
+      area: 'Tecnologia',
+      members: 12,
+      nextMeeting: 'Amanhã, 14:00'
+    },
+    {
+      title: 'Grupo de Leitura: IA Generativa',
+      institution: 'USP',
+      area: 'Computação',
+      members: 8,
+      nextMeeting: 'Sexta, 19:00'
+    },
+    {
+      title: 'Engenharia de Software Ágil',
+      institution: 'UFMG',
+      area: 'Tecnologia',
+      members: 7,
+      nextMeeting: 'Segunda, 10:00'
+    },
+    {
+      title: 'Cálculo 2 - Resolução de Listas',
+      institution: 'Unicamp',
+      area: 'Exatas',
+      members: 25,
+      nextMeeting: 'Hoje, 16:00'
+    }
+  ]);
+}
---
+++ src/app/feed/interface/group_card.interface.ts
@@ -0,0 +1,9 @@
+interface group_card {
+  title: string;
+  institution: string;
+  area: string;
+  members: number;
+  nextMeeting: string;
+}
+
+
