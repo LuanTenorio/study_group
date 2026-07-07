cfec6b9c7b168ec1107f3b6ac41cab3046f4cddd
caue-oliver <caue.ooliver@gmail.com>
2026-07-06 22:01:22 -0300

    feat: integração meus grupos

--- src/app/mygroups.component/mygroups.component.ts
+++ src/app/mygroups.component/mygroups.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { group_card } from '../feed/interface/group_card.interface';
+import { AuthService } from '../auth/service/auth.service';
 
 @Component({
   selector: 'app-my-groups',
@@ -13,22 +14,22 @@ import { group_card } from '../feed/interface/group_card.interface';
 })
 export class MyGroupsComponent implements OnInit {
   
-  // Signal para guardar os grupos do utilizador
   myGroups = signal<group_card[]>([]);
-
-  // Injeção do HttpClient para aceder ao backend
   private readonly http = inject(HttpClient);
-
-  // ID do utilizador (mockado para testes até termos o sistema de login pronto)
-  private readonly currentUserId = 1;
+  private readonly authService = inject(AuthService); 
 
   ngOnInit() {
     this.loadMyGroups();
   }
 
   loadMyGroups() {
-    // Chamada à nova rota que vamos criar no backend NestJS
-    const apiUrl = `http://localhost:3000/group/my-groups/${this.currentUserId}`;
+    const currentUser = this.authService.currentUser(); 
+
+    if (!currentUser) {
+      this.myGroups.set([]);
+      return; 
+    }
+    const apiUrl = `http://localhost:3000/group/my-groups/${currentUser.id}`;
     
     this.http.get<group_card[]>(apiUrl).subscribe({
       next: (dados) => {
@@ -36,8 +37,6 @@ export class MyGroupsComponent implements OnInit {
       },
       error: (erro) => {
         console.error('Erro ao buscar os meus grupos:', erro);
-        // Se houver erro ou não encontrar nada, garantimos que a lista fica vazia
-        // para exibir o "Empty State" bonitinho que criámos no HTML
         this.myGroups.set([]);
       }
     });
